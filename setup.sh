7eb94395134597c4658c61b1c834e5b0077bd78b
manumissi0n <gallimanuel@gmail.com>
2021-11-30 14:24:47 +0100

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #! bin/bash
 
-git clone https://github.com/marcomasulli/flasklet.git
+#git clone https://github.com/marcomasulli/flasklet.git
 sudo apt install python3-pip python3.8-venv
 cd flasklet
 python3 -m venv .env
